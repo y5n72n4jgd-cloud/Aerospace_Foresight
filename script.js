cd79efde49f1fec9e00add047359fe78727f725e
Arav Shete <y5n72n4jgd@privaterelay.appleid.com>
2026-06-22 11:21:56 +0900

    Add console logs for card elements and click events

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ console.log("Aerospace Foresight loaded.");
 
 const cards = document.querySelectorAll(".card");
 
+console.log(cards);
+
 cards.forEach(card => {
     card.addEventListener("click", () => {
+        console.log("clicked");
         card.classList.toggle("active");
     });
 });
