086b21892ce07126e7c730a3540ff6efded5a09b
Arav Shete <y5n72n4jgd@privaterelay.appleid.com>
2026-06-22 11:29:43 +0900

    Remove console logs from script.js

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ console.log("Aerospace Foresight loaded.");
 
 const cards = document.querySelectorAll(".card");
 
-console.log(cards);
-
 cards.forEach(card => {
     card.addEventListener("click", () => {
-        console.log("clicked");
         card.classList.toggle("active");
     });
 });
