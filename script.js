266db9a6a49c8400971ae5c90bced38ebb128efe
Arav Shete <y5n72n4jgd@privaterelay.appleid.com>
2026-06-30 11:36:47 +0900

    Add cursor glow effect on mouse movement

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ cards.forEach(card => {
         card.classList.toggle("active");
     });
 });
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+
+glow.style.left=e.clientX+"px";
+glow.style.top=e.clientY+"px";
+
+});
